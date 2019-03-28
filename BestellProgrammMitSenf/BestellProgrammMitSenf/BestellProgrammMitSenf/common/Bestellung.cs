a61030299bf2bd04204bd537682f842d29cfc0cb
Thomas Rosengrün <thomas.rosengruen94@gmail.com>
2019-03-28 10:42:54 +0100

    DBAccess Update

---
+++ BestellProgrammMitSenf/BestellProgrammMitSenf/BestellProgrammMitSenf/common/Bestellung.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Types;
+
+namespace BestellProgrammMitSenf.common
+{
+    public class Bestellung
+    {
+        public long BestellNr;
+        public OracleDate Bestelldatum;
+        public long KundenNr;
+        public string Beschreibung; //Name Vorname Speise
+        public string Groesse;
+        public long Anzahl;
+        public string Extras;
+    }
+}
