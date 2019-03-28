a243d07baf9013b94db71a3cfc26aa3214131cd7
Thomas Rosengrün <thomas.rosengruen94@gmail.com>
2019-03-28 13:14:22 +0100

    Nicht eingebundene Datei hinzugefügt.

---
+++ BestellProgrammMitSenf/BestellProgrammMitSenf/BestellProgrammMitSenf/common/BestellElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestellProgrammMitSenf.common
+{
+    public class BestellElement
+    {
+        public long SpeiseNr;
+        public long SpeiseGroessenID;
+        public long Anzahl;
+        public List<long> ExtraZutaten;
+    }
+}
